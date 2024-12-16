dfc8b005f84c30cee4897c78ac6767c40fc784db
BBurakUzun <bugraburakuzun@hotmail.com>
2024-12-16 13:51:42 +0300

    lib commit

--- src/main/java/org/example/veritabaniprojesi/HelloController.java
+++ src/main/java/org/example/veritabaniprojesi/HelloController.java
@@ -1,14 +1,32 @@
 package org.example.veritabaniprojesi;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Text signUpText;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void onSignIn(ActionEvent event) {
+        if (usernameField.getText().equals("asdas") && passwordField.getText().equals("123")) {
+            System.out.println("sa");
+        }
     }
 }
